34790309bc74af11f610701f57926e8e849ea72b
Nishant Hegde <55502735+NIshant-Hegde@users.noreply.github.com>
2019-11-04 16:08:34 +0530

    Update Identify_the_sentiments.py

--- Identifying_the_sentiments_Analytics_Vidhya_Challenge/Identify_the_sentiments.py
+++ Identifying_the_sentiments_Analytics_Vidhya_Challenge/Identify_the_sentiments.py
@@ -1,15 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 path = "C:\\Users\\Nishant\\Desktop\\Data Science notes\\Data Science Projects\\"
 train_values = "train_2kmZucJ.csv"
-
-
-# In[2]:
-
 
 import numpy as np
 import pandas as pd
@@ -24,50 +14,25 @@ from ipywidgets import FloatProgress
 from IPython.display import display
 
 
-# In[3]:
-
-
 xdf = pd.read_csv(path + train_values).fillna(' ')
-
-
-# In[4]:
-
-
 xdf.head(25)
+xdf.describe()                                       #Descriptive Statistics
 
-
-# In[5]:
-
-
-xdf.describe()
-
-
-# In[6]:
-
-
-xdf['tweet'] = xdf['tweet'].str.lower()
-
-
-# In[7]:
-
-
+xdf['tweet'] = xdf['tweet'].str.lower()              #Converting the text to lower case
 xdf.head()
 
 
-# In[8]:
+from nltk.stem import PorterStemmer                  #Using the porterstemmer library to convert each word into its root form
 
+ps = PorterStemmer()                        
 
-from nltk.stem import PorterStemmer
-
-ps = PorterStemmer()
-
-def root_word(x):
+def root_word(x):                                    #A user defined function to convert each word of the text feature into root-word 
     root = []
     for phrase in x.split():
-        root.append(ps.stem(phrase))
+        root.append(ps.stem(phrase))                 #Converting into root form and appending to the list
     return " ".join(root)
 
-def preprocess(ReviewText):     #Function 
+def preprocess(ReviewText):                          #A user defined function to preprocess the text feature 
     ReviewText = ReviewText.str.replace(",", " ")
     ReviewText = ReviewText.str.replace("!", " ")
     ReviewText = ReviewText.str.replace("-", " ")
@@ -83,111 +48,65 @@ def preprocess(ReviewText):     #Function
     ReviewText = ReviewText.str.replace("#", " ")
     ReviewText = ReviewText.str.replace("?", " ")
     ReviewText = ReviewText.str.replace("^", " ")
-    ReviewText = ReviewText.str.replace("*", " ")
+    ReviewText = ReviewText.str.replace("*", " ")    #Replacing the punctuation marks
     
     return ReviewText
 
-xdf['tweet'] = preprocess(xdf['tweet']) 
+xdf['tweet'] = preprocess(xdf['tweet'])              #Applying the user defined function preprocesss on text feature column
 xdf["tweet"] = xdf["tweet"].str.lower()
 
-xdf["tweet"] = xdf["tweet"].map(lambda x: root_word(x)) 
-
-
-# In[9]:
-
-
+xdf["tweet"] = xdf["tweet"].map(lambda x: root_word(x)) #Applying the user defined function root_word on text feature column
 xdf.head(30)
-
-
-# In[10]:
-
 
 xdf.drop('id', axis = 1, inplace = True)
 
 
-# In[11]:
-
-
-xdf.head()
-
-
-# In[12]:
-
-
-from textblob import TextBlob
+from textblob import TextBlob           #TextBlob for feature engg(Calculating sentiment polarity and total words in each text row)
 
 #Feature Engineering
 xdf['polarity'] = xdf['tweet'].map(lambda text: TextBlob(text).sentiment.polarity) #Creating a new row for sentiment_polarity
 xdf['review_len'] = xdf['tweet'].astype(str).apply(len)                            #Calculating the number of characters in review column   
 xdf['word_count'] = xdf['tweet'].apply(lambda x: len(str(x).split())) 
-
-
-# In[13]:
-
-
 xdf.head()
 max(xdf['review_len'])
 
-
-# In[14]:
-
-
-train_y = xdf['label']
+train_y = xdf['label']                                                 #Creating the labels array
 sentences = xdf['tweet']
 
-
-# In[15]:
-
-
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import Tokenizer              #To tokenize the text
+from tensorflow.keras.preprocessing.sequence import pad_sequences      #To pad uneven length sequences
 
 #Hyper parameters
 num_words = 10000
 pad_type = 'post'
-oov_token = "<OOV>"
+oov_token = "<OOV>"                                                    #Out of vocabulary token
 embedding_dim = 16
 max_length = 250
 
 
-# In[16]:
+tokenizer = Tokenizer(num_words = num_words, oov_token = oov_token)    #Creating a tokenizer object
+tokenizer.fit_on_texts(sentences)                                      #Using the method fit_on_texts() to tokenize the text feature
+word_index = tokenizer.word_index                                      #Displaying the word-index
 
-
-tokenizer = Tokenizer(num_words = num_words, oov_token = oov_token)
-tokenizer.fit_on_texts(sentences)
-word_index = tokenizer.word_index
-
-
-# In[17]:
-
-
-sequences = tokenizer.texts_to_sequences(sentences)
+ con
+sequences = tokenizer.texts_to_sequences(sentences)                    #Using the texts_to_sequences() method to convert the tokens into sequences
 padded_sequences = pad_sequences(sequences, maxlen = max_length, padding = 'post', truncating = 'post')
-
-
-# In[18]:
+                                                                       #Padding the sequences. Padding type is "post".
 
 
 pdf = pd.DataFrame(padded_sequences)
 
-
-# In[19]:
-
-
 from sklearn.model_selection import train_test_split
 
-train_x, test_x, train_y, test_y = train_test_split(pdf, train_y, test_size = 0.1, random_state = 10, shuffle = True)
-
-
-# In[20]:
-
-
+train_x, test_x, train_y, test_y = train_test_split(pdf, train_y, test_size = 0.1, random_state = 10, shuffle = True) 
+                                                                       #Splitting the dataset into train_data and test_data
+    
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Embedding, GlobalAveragePooling1D
 
-model = Sequential([
+model = Sequential([                                                   #Creating a sequential model that uses embeddings of dimension 16
     Embedding(num_words, embedding_dim, input_length = max_length),
-    GlobalAveragePooling1D(),
+    GlobalAveragePooling1D(),                                          #GlobalAveragePooling1D() instead of Flatten() as the former results in less trainable parameters
     Dense(16, activation = 'relu'),
     Dense(1, activation = 'sigmoid')
 ])
@@ -196,93 +115,32 @@ model.compile(loss = 'binary_crossentropy', optimizer = 'adam', metrics = ['accu
 
 history = model.fit(train_x, train_y, epochs = 50, validation_data = (test_x, test_y), verbose = 1)
 
-
-# In[21]:
-
-
 test_file = "test_oJQbWVk.csv"
 sub_file = "sample_submission_LnhVWA4.csv"
 
 tdf = pd.read_csv(path + test_file).fillna(' ')
 sdf = pd.read_csv(path + sub_file).fillna(' ')
-
 tdf.head()
-
-
-# In[22]:
-
 
 tdf['tweet'] = preprocess(tdf['tweet']) 
 tdf["tweet"] = tdf["tweet"].str.lower()
-
 tdf["tweet"] = tdf["tweet"].map(lambda x: root_word(x)) 
-
 ID = tdf['id']
-
-
-# In[23]:
-
-
 tdf.drop('id', axis = 1, inplace = True)
+test_sequences = tokenizer.texts_to_sequences(tdf['tweet'])           #Tokenizing the test data 
+padded_test = pad_sequences(test_sequences, padding = 'post', truncating = 'post', maxlen = max_length)  #Padding the test data
 
 
-# In[24]:
+preds = model.predict(padded_test)       #Predicitions
 
-
-test_sequences = tokenizer.texts_to_sequences(tdf['tweet'])
-
-
-# In[25]:
-
-
-padded_test = pad_sequences(test_sequences, padding = 'post', truncating = 'post', maxlen = max_length)
-
-
-# In[26]:
-
-
-preds = model.predict(padded_test)
-
-
-# In[27]:
-
-
-for i, values in enumerate(preds):
+for i, values in enumerate(preds):       #Coverting the prediction probabilities to binary classes
     if values > 0.5:
         preds[i] = 1
     else:
         preds[i] = 0
 
-
-# In[28]:
-
-
-preds = pd.DataFrame(preds, columns = ['label'])
+preds = pd.DataFrame(preds, columns = ['label'])     #Creating a dataframe of the predictions for submission
 preds['id'] = tdf['id']
 preds = preds[['id', 'label']]
 preds.head()
-
-
-# In[ ]:
-
-
 preds.to_csv(path + "submit.csv", index = False)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-8296045159
-
